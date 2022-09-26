3028bed474edfd0b618032c215c82fcbe023508e
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-09-26 09:39:32 +0200

    feat[password_keeper]: working bottom navigation bar, added blank settings screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:password_keeper/model/Credentials.dart';
-import 'package:password_keeper/repository/credentials_repository.dart';
-import 'package:password_keeper/view/home/credentials_row.dart';
-import 'package:password_keeper/view/home/top_caption.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:password_keeper/view/home/home.dart';
+import 'package:password_keeper/view/settings/settings.dart';
 
 void main() {
   runApp(const PasswordKeeperApp());
@@ -17,135 +14,25 @@ class PasswordKeeperApp extends StatefulWidget {
 }
 
 class _PasswordKeeperAppState extends State<PasswordKeeperApp> {
-  CredentialsRepository credentialsRepository = CredentialsRepository();
-  bool loadingCredentials = true;
-
-  void getCredentials() async {
-    var result = await credentialsRepository.getCredentials();
-    setState(() {
-      credentials = result;
-      loadingCredentials = false;
-    });
-  }
-
-  List<Credentials> credentials = [];
-
-  @override
-  void initState() {
-    super.initState();
-    getCredentials();
-  }
+  int currentScreen = 0;
+  final screens = [
+    const Home(),
+    const Settings(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // routes: ,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Password Keeper'),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 20),
-              child: GestureDetector(
-                onTap: () {},
-                child: const Icon(
-                  Icons.search,
-                  size: 28,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 20),
-              child: GestureDetector(
-                onTap: () {},
-                child: const Icon(
-                  Icons.edit,
-                  size: 28,
-                ),
-              ),
-            )
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Center(
-            child: Column(
-              children: [
-                const TopCaption(),
-                loadingCredentials
-                    ? Column(
-                        children: [
-                          const Text('Loading data'),
-                          const SizedBox(
-                            height: 30,
-                          ),
-                          LoadingAnimationWidget.staggeredDotsWave(
-                              color: Colors.black, size: 70),
-                        ],
-                      )
-                    : credentials.isEmpty
-                        ? const Text('No passwords yet')
-                        : Column(
-                            children: credentials
-                                .map(
-                                  (e) => CredentialsRow(
-                                    credentials: e,
-                                    delete: () {
-                                      setState(() {
-                                        credentials.remove(e);
-                                      });
-                                    },
-                                  ),
-                                )
-                                .toList(),
-                          ),
-              ],
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.lightGreen,
-          // TODO: sheet for adding credentials
-          onPressed: () {
-            setState(() {
-              credentials.add(
-                Credentials(
-                    websiteURL: 'websiteNew.com',
-                    login: 'login',
-                    passwordHash: 'passNew'),
-              );
-            });
-          },
-          child: const Icon(
-            Icons.add,
-          ),
-        ),
-        drawer: Drawer(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 80,
-                ),
-                const Text(
-                  'Hello user!',
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
-                ),
-                const Divider(),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('log out'),
-                )
-              ],
-            ),
-          ),
+        body: IndexedStack(
+          index: currentScreen,
+          children: screens,
         ),
         bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentScreen,
+          onTap: (index) => setState(() {
+            currentScreen = index;
+          }),
           items: const [
             BottomNavigationBarItem(
               icon: Icon(Icons.home),

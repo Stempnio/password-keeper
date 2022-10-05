0fa198d612f7e57a7dd24b7a61b955d2b024ca68
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-10-05 20:31:24 +0200

    add[password_keeper]: settings basic screen with mockup form fields

--- lib/view/settings/settings.dart
+++ lib/view/settings/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animated_toggle_switch/animated_toggle_switch.dart';
 
 class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
@@ -8,10 +9,100 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('asdf'),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(30, 40, 30, 30),
+      child: Form(
+        key: _formKey,
+        child: Center(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: const [
+                  Text(
+                    "Settings",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Spacer(),
+                ],
+              ),
+              const Divider(),
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Enter new nickname',
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Nickname can not be empty';
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          hintText: 'Enter new password',
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Password can not be empty';
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Success')),
+                            );
+                          }
+                        },
+                        child: const Text('submit'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const Divider(),
+              Row(
+                children: [
+                  const Text(
+                    "Dark mode",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Spacer(),
+                  AnimatedToggleSwitch<String>.rolling(
+                    values: const ["on", "off"],
+                    current: "on",
+                    height: 30,
+                  )
+                ],
+              ),
+              const Divider(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }

c2db31d237e6fe626e3b3dbcea557e78a7459676
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-10-27 18:33:45 +0200

    add[password_keeper]: credentials bloc and repository providers in main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
+import 'package:credentials_repository/credentials_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:password_keeper/app/app.dart';
+import 'package:password_keeper/credentials/bloc/credentials_bloc.dart';
+import 'package:password_keeper/theme/theme.dart';
 
-void main() {
-  runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final credentialsRepository = CredentialsRepository();
+
+  runApp(RepositoryProvider(
+    create: (context) => credentialsRepository,
+    child: MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => CredentialsBloc(
+            credentialsRepository: credentialsRepository,
+          )..add(CredentialsFetched()),
+        ),
+        BlocProvider(
+          create: (context) => ThemeCubit(),
+        ),
+      ],
+      child: const App(),
+    ),
+  ));
 }

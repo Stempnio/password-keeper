9b82729a50c4cfa3b8b1a63286d09895f6a4665d
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-10-28 21:29:18 +0200

    add[password_keeper]: authenticate method, check for new password length (minimum 5)

--- packages/authentication_repository/lib/src/authentication_repository.dart
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -1,13 +1,20 @@
 import 'models/models.dart';
 
 class AuthenticationRepository {
-  AuthenticationData authenticationData = AuthenticationData(passCode: "0000");
+  AuthenticationData _authenticationData = AuthenticationData(passCode: "0000");
 
   Future<AuthenticationData> getAuthenticationData() async {
-    return authenticationData;
+    return _authenticationData;
   }
 
   Future<void> setAuthenticationData(AuthenticationData newAuthData) async {
-    authenticationData = newAuthData;
+    if (newAuthData.passCode.length < 5) {
+      throw Exception(["Password too short!"]);
+    }
+    _authenticationData = newAuthData;
+  }
+
+  Future<bool> authenticate(AuthenticationData authenticationData) async {
+    return _authenticationData == authenticationData;
   }
 }

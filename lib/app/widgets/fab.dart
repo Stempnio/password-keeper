39c9d5852ca028d0fce406660c4e122d55036bdb
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-11-01 11:41:52 +0100

    style[password_keeper]: change floating action button color

--- lib/app/widgets/fab.dart
+++ lib/app/widgets/fab.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:password_keeper/edit_credentials/edit_credentials.dart';
 import 'package:password_keeper/credentials/credentials.dart';
 import 'package:credentials_repository/credentials_repository.dart';
+import 'package:password_keeper/theme/theme.dart';
 
 class Fab extends StatelessWidget {
   const Fab({Key? key}) : super(key: key);
@@ -10,6 +11,9 @@ class Fab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
+      backgroundColor: context.watch<ThemeCubit>().isDark
+          ? Colors.lightGreenAccent.withOpacity(0.85)
+          : Colors.lightGreen,
       heroTag: 'add_credentials',
       onPressed: () => Navigator.push(
         context,
